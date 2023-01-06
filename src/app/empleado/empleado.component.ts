70504d8dc57c01cba057c88aff05d8a8b2546170
Andy Castillo <andy.castillopalma@outlook.com>
2023-01-05 22:43:08 -0500

    inline components

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-empleado',
   templateUrl: './empleado.component.html',
+  //template: "<p>empleado works!</p>",
   styleUrls: ['./empleado.component.css']
+  //styles: ["p {background-color: red;}"]
 })
 export class EmpleadoComponent {
 
